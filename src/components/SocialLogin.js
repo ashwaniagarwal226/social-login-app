0f57a48fc28e2a44d676c4a7ed185f4657243639
Ashwani <ashwaniagarwal226@gmail.com>
2020-08-15 11:42:15 +0530

    css Added

--- src/components/SocialLogin.js
+++ src/components/SocialLogin.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import SocialButton from './SocialButton';
-
+import '../App.css';
 class SocialLogin extends Component {
     render() {
 
         return (
-            <div>
-                <SocialButton type="facebook" />
-                <SocialButton type="twitter" />
-                <SocialButton type="github" />
+            <div className="container">
+                <div class="row">
+                    <div class="col">
+                        <SocialButton type="facebook" />
+                        <SocialButton type="twitter" />
+                        <SocialButton type="github" />
+                    </div>
+                </div>
             </div>);
     };
 };
