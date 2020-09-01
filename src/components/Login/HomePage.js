7bb9088deb0aeedf5388fa2595fcfdd979face67
Ashwani <ashwaniagarwal226@gmail.com>
2020-09-01 07:35:19 +0530

    homepage

--- src/components/Login/HomePage.js
+++ src/components/Login/HomePage.js
@@ -6,7 +6,16 @@ class HomePage extends Component {
 
         return (
             <div className="row">
-            
+                <form action={process.env.REACT_APP_URL + "connect/facebook"} method="post">
+                    <div class="formInfo">
+                        <p>
+                            Spring Social Showcase is connected to your facebook account.
+                            Click the button if you wish to disconnect.
+    </p>
+                    </div>
+                    <button type="submit">Disconnect</button>
+                    <input type="hidden" name="_method" value="delete" />
+                </form>
             </div >
         );
     };
