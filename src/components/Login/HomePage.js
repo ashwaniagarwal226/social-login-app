b7e0efb38d4c22f0bca3163602208cf3ea181973
Ashwani <ashwaniagarwal226@gmail.com>
2020-09-01 07:33:55 +0530

    homepage

---
+++ src/components/Login/HomePage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+
+    render() {
+
+        return (
+            <div className="row">
+            
+            </div >
+        );
+    };
+
+
+}
+
+export default HomePage;
--- src/components/Urls.js
+++ src/components/Urls.js
@@ -1,10 +1,15 @@
 import SocialLogin from "./Login/SocialLogin";
+import HomePage from "./Login/HomePage";
 
 const routes = [
     {
         path: "/",
         exact: true,
         component: SocialLogin
+    },
+    {
+        path: "/home",
+        component: HomePage
     }
 
 ];
